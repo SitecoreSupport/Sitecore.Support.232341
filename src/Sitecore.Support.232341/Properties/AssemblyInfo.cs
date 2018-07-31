ab4c9075364c1ec8d82f704999b90ec6488edb6b
DK\dmsa <dmsa@sitecore.net>
2018-07-31 12:44:01 +0300

    The origin code of the "LanguageSwitcher.js" file is added.

---
+++ src/Sitecore.Support.232341/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.232341")]
+[assembly: AssemblyProduct("Sitecore.Support.232341")]
+[assembly: ComVisible(false)]
